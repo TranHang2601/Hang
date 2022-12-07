3419259a55da07df547195ac4a2e4596c4192b5b
TranHang2601 <73451815+TranHang2601@users.noreply.github.com>
2022-12-07 16:07:17 +0700

    Initial commit

---
+++ src/components/Pages.tsx
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { PageTransition } from "@steveeeie/react-page-transition";
+import { pages } from "../pages";
+import { AppContext } from "./AppContext";
+import { Page } from "./Page";
+
+function Pages() {
+  const location = useLocation();
+  const appContext = useContext(AppContext);
+
+  if (appContext) {
+    return (
+      <PageTransition
+        preset={appContext.preset}
+        transitionKey={location.pathname}
+        enterAnimation={appContext.enterAnimation || ""}
+        exitAnimation={appContext.exitAnimation || ""}
+      >
+        <Routes location={location}>
+          {pages.map((page, index) => {
+            return (
+              <Route
+                key={index}
+                path={page.path}
+                element={<Page page={page} />}
+              />
+            );
+          })}
+          <Route path="/" element={<Navigate to="/page-1" />} />
+        </Routes>
+      </PageTransition>
+    );
+  }
+
+  return null;
+}
+
+export { Pages };
